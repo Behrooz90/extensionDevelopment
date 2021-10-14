353730bebc8e1de604e53de568f6095b57e2059b
Emily Schroeder <erschroeder@knox.edu>
2021-10-14 01:28:25 -0500

    adding character button

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.getElementById ("char").addEventListener ("click", initChar, false);
+
+function initChar() {
+  alert("Hello! I am an alert box!!");
+  window.open('Character_B.png','','height=150,width=190,scrollbars=no,titlebar=no,menubar=no,channelmode=yes');
+}
