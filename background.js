7af1439ea0cda37a032d5f609a55a99f5085c102
bethlehem-code <bamulualem@knox.edu>
2021-10-15 13:35:09 -0500

    Saving changes to drop down

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
+
+//The button clicked and action listener will work only when borwserAction has only browser icon as a
 console.log('background running');
-chrome.browserAction.onClicked.addListener(buttonClicked);
+chrome.browserAction.onClicked.addListener(buttonClicked)
+document.getElementsByClassName("buttons").addListener.onClicked(buttonClicked)
 
 function buttonClicked(){
-    console.log("button clicked!")
+    console.log("icon clicked!");
 }
