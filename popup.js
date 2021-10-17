cf25136cabb67f87c809436ede6c0e8cb53551e6
bethlehem-code <bamulualem@knox.edu>
2021-10-17 16:05:24 -0500

    Drop down in menue for further options. There is an bug with resizing the background aftere expansion.

---
+++ popup.js
@@ -0,0 +1,28 @@
+// console.log('content script-js')
+
+// const clock= document.createElement('div')
+// clock.style.color= 'red'
+// clock.style.fontSize='5rem'
+// clock.innerText= 'CLOCK'
+    
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    // if (content.style.display === "block") {
+    //   content.style.display = "none";
+    // } else {
+    //   content.style.display = "block";
+    // }
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+  });
+}
+
+
+//document.body.append(clock)
