05d6cce9e5d409c117058f346b43526fa54cf1db
bethlehem-code <bamulualem@knox.edu>
2021-10-26 10:46:34 -0500

    Include manifest file with web acessible resources and a load button in popup.js to have the image appear.

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55075fea846d36d1f280db47b36effda2781c6ce177a0671f0b52cbbd09b52b3
-size 380
+oid sha256:f66e0ba334c537773ccbb2eb5f7689f0eb98403b9665475cedcdab739ebdd1b3
+size 321
--- popup.js
+++ popup.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39dcd673ce0641ba01f0c840503a864b8845af357845007aad8c1024518dc256
-size 2438
+oid sha256:af3218a3c30e2db86ce0dfa2dcf5b7682bd4b815abc7ef03585eaab0a4777df2
+size 2377
