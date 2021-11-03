0fb9d19f55a3ed8ab3a3ad9f8293192a53ff0c2c
Emily Schroeder <erschroeder@knox.edu>
2021-11-03 01:44:23 -0500

    formatting

--- popup.js
+++ popup.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f113c4582307858dc2e7ec5ec7909f3d5c61ce176a683dca6387c56b185f9894
-size 2814
+oid sha256:cf6ebfa7994a2ec2205f6d97245ecdb3c73e458ccead7842d251282dd7c4cf6b
+size 2886
